f656e5c7fdae5837b1e17eca23baa714e5f19332
Rubén AS <rualsi@hotmail.com>
2024-09-26 17:01:14 +0200

    Actualización menor

--- utils/emails/forgottenPass.php
+++ utils/emails/forgottenPass.php
@@ -14,7 +14,13 @@ function forgottenPassEmail($swimmer) {
 
     $body .= '<p>Un saludo,</p>';
 
+    $body .= '<hr>';
+
     $body .= '<p>Equipo web CN Escualos.</p>';
+
+    $body .= '<p><img src="'.(isset($_SERVER['HTTPS']) ? 'https' : 'http').'://'.$_SERVER['HTTP_HOST'].'/escualos/public/img/logo_escualos.svg" style="width:150px"></p>';
+
+    $body .= '<hr><p style="font-size:10px">Este mensage se dirige exclusivamente a su destinatario/a. Puede contener información confidencial sometida a secreto profesional o cuya divulgación esté prohibida, en virtud de la legislación vigente. No está permitida la divulgación, copia o distribución a terceros sin la autorización previa del remitente. Si has recibido este mensage por error, rogamos que nos lo comuniques inmediatamente por esta misma vía y procedas a su destrucción.</p><hr>';
 
     return [
         'subject' => $subject,
